263b24c2488c72485b99a7ab6cba425a82ae8e38
Grammatopoulos Athanasios-Vasileios <gramthanos@gmail.com>
2019-06-01 01:26:00 +0300

    Project discontinued

--- script.js
+++ script.js
@@ -3,46 +3,6 @@
  * Copyright 2018
  * All rights reserved.
  */
-	
-var version = "v2.2.3";
-
-// Downloads function
-(function(){
-	// Links
-	var links = {};
-	links.base = 'https://github.com/DinoDevs/Everwing-Hacks/',
-	links.crx = links.base + 'releases/download/' + version + '/EverWingHacks.' + version + '.crx';
-	links.xpi = links.base + 'releases/download/' + version + '/EverWingHacks.' + version + '.xpi';
-	links.nex = links.base + 'releases/download/' + version + '/EverWingHacks.' + version + '.nex';
-	//links.chromeStore = 'https://chrome.google.com/webstore/detail/everwing-hacks/fbingkbgnhkfpmffjiekekmedohpmfef';
-	
-	// Get elements
-	var button = document.getElementById("download-button");
-	var info = document.getElementById("browser-info");
-	
-	// Check support
-	if (bowser.firefox) {
-		button.href = links.xpi;
-		button.className = 'button';
-		info.textContent = 'for ' + bowser.name + ' ' + bowser.version;
-	}
-	else if (bowser.chrome || bowser.chromium) {
-		button.href = links.crx;
-		button.setAttribute('target', '_blank');
-		button.className = 'button';
-		info.textContent = 'for ' + bowser.name + ' ' + bowser.version;
-	}
-	else if (bowser.opera) {
-		button.href = links.nex;
-		button.setAttribute('target', '_blank');
-		button.className = 'button';
-		info.textContent = 'for ' + bowser.name + ' ' + bowser.version;
-	}
-	else {
-		info.textContent = bowser.name + ' ' + bowser.version + ' is not yet supported';
-	}
-	button.getElementsByTagName('span')[0].appendChild(document.createTextNode(' ' + version));
-})();
 
 // Page functions
 (function(){
@@ -122,14 +82,15 @@ var version = "v2.2.3";
 			"code" : (function(){
 				// Define code
 				var code = [
-					'EverWing-Hacks ' + version + ' by dinodevs # ' + (new Date().toGMTString().replace(',', '')) + ' ' + bowser.osname + ' ' + bowser.osversion, {type:'br'},
+					'EverWing-Hacks by dinodevs # ' + (new Date().toGMTString().replace(',', '')) + ' ' + bowser.osname + ' ' + bowser.osversion, {type:'br'},
+					'This project was discontinued. Thank you for your support.', {type:'br'},
 					{type:"br"},
-					'This extension is intended to be used ONLY for educational purposes. It was created to show how easy it is to cheat on a popular game just by running some simple commands on the browser\'s console.', {type:'br'},
+					'This extension was intended to be used ONLY for educational purposes. It was created to show how easy it is to cheat on a popular game just by running some simple commands on the browser\'s console.', {type:'br'},
 					{type:'br'},
-					'This software comes with ABSOLUTELY NO WARRANTY. By using it your account may and should get banned for cheating.', {type:'br'},
+					'This software came with ABSOLUTELY NO WARRANTY. By using it your account chould get banned for cheating.', {type:'br'},
 					{type:'br'},
 					'Last login: ' + (new Date().toGMTString().replace(',', '')) + ' from ' + bowser.name + ' ' + bowser.version, {type:'br'},
-					{type:'command', text:'root@localhack'}, ' ', {type:'dir', text:'~ #'}, ' ', {type:'cursor', text:'_'}
+					{type:'command', text:'root@localhack'}, ' ', {type:'dir', text:'~ #'}, ' ./check_other_forks.sh', {type:'cursor', text:'_'}
 				];
 				for (var i = 0; i < code.length; i++) {
 					if (typeof code[i] === 'string') {
@@ -148,7 +109,6 @@ var version = "v2.2.3";
 	setTimeout(function(){
 		typeHearts(function(){
 			typeTerminal();
-			document.getElementById('downloads').style.opacity = 1;
 			document.getElementById('footer').style.opacity = 1;
 		});
 	}, 500);
